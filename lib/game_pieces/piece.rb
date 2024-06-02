aa98f3d81c79534df494f3444a3572eefd71fcb4
Everesh <everesh@proton.me>
2024-06-02 17:59:05 +0200

    Fix Piece class declaration

--- lib/game_pieces/piece.rb
+++ lib/game_pieces/piece.rb
@@ -1,8 +1,13 @@
-def Piece
+class Piece
 
-    attr_reader :color
+  attr_reader :color
 
-    def initialize(color)
-        @color = color
-    end
+  def initialize(color)
+    @color = color
+  end
+
+  def symb
+    color == 'white' ? self.class::WHITE : self.class::BLACK
+  end
+
 end
