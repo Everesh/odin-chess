5e71eb9c651f49364d1a6960d763d6aab79fb986
Everesh <everesh@proton.me>
2024-06-02 19:49:00 +0200

    Depreciate move

--- lib/board.rb
+++ lib/board.rb
@@ -55,53 +55,8 @@ class Board
     puts column_legend
   end
 
-  def move(str)
-    arr = str.chars
-    piece = identify_piece(arr)
-    target_col = arr[0].ord - 97
-    target_row = 8 - arr[1].to_i
-
-    piece_moves = piece::MOVES
-    piece_moves.each do |move|
-      if active_player == 'white'
-        current_row = target_row + move[1]
-        current_col = target_col + move[0]
-      else
-        current_row = target_row - move[1]
-        current_col = target_col - move[0]
-      end
-
-      next if current_row < 0 || current_row > 7 || current_col < 0 || current_col > 7
-      next unless board[current_row][current_col].is_a?(piece) && board[current_row][current_col].color == active_player && board[target_row][target_col] == ' '
-      board[target_row][target_col] = board[current_row][current_col]
-      board[current_row][current_col] = ' '
-      self.active_player = active_player == 'white' ? 'black' : 'white'
-      return true
-    end
-    false
-  end
-
   private
 
   attr_writer :board, :history, :active_player
 
-  def identify_piece(arr)
-    piece = arr.shift
-    case piece
-    when 'K'
-      piece = King
-    when 'Q'
-      piece = Queen
-    when 'R'
-      piece = Rook
-    when 'B'
-      piece = Bishop
-    when 'N'
-      piece = Knight
-    else
-      arr.unshift(piece)
-      piece = Pawn
-    end
-    piece
-  end
 end
