a10c6036c160e0055232939e24f27041fed3d896
Everesh <everesh@proton.me>
2024-06-02 21:13:38 +0200

    Alter inner board representation

--- lib/board.rb
+++ lib/board.rb
@@ -18,25 +18,25 @@ class Board
 
   def set
     8.times do |index|
-      board[1][index] = Pawn.new('black')
-      board[-2][index] = Pawn.new('white')
+      board[1][index] = Pawn.new('white')
+      board[-2][index] = Pawn.new('black')
       board[2..-3].each { |row| row[index] = ' ' }
       case index
       when 0, 7
-        board[0][index] = Rook.new('black')
-        board[-1][index] = Rook.new('white')
+        board[0][index] = Rook.new('white')
+        board[-1][index] = Rook.new('black')
       when 1, 6
-        board[0][index] = Knight.new('black')
-        board[-1][index] = Knight.new('white')
+        board[0][index] = Knight.new('white')
+        board[-1][index] = Knight.new('black')
       when 2, 5
-        board[0][index] = Bishop.new('black')
-        board[-1][index] = Bishop.new('white')
+        board[0][index] = Bishop.new('white')
+        board[-1][index] = Bishop.new('black')
       when 3
-        board[0][index] = Queen.new('black')
-        board[-1][index] = Queen.new('white')
+        board[0][index] = Queen.new('white')
+        board[-1][index] = Queen.new('black')
       when 4
-        board[0][index] = King.new('black')
-        board[-1][index] = King.new('white')
+        board[0][index] = King.new('white')
+        board[-1][index] = King.new('black')
       end
     end
   end
@@ -48,7 +48,7 @@ class Board
     puts separator
     8.times do |row|
       print "#{8 - row} |"
-      8.times { |column| print " #{board[row][column] == ' ' ? ' ' : board[row][column].symb} |" }
+      8.times { |column| print " #{board[7 - row][column] == ' ' ? ' ' : board[7 - row][column].symb} |" }
       puts " #{8 - row}"
       puts separator
     end
